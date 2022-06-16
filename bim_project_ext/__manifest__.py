473a3fab9b455c15a710ddd4ed57ba8e9a89602b
Luis Felipe Paternina <luisfpaternina93@gmail.com>
2022-06-16 07:49:25 -0500

    bim ext

---
+++ bim_project_ext/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'BIM project ext',
+
+    'version': '14.0.0.0',
+
+    'author': "Process Control",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "https://www.processcontrol.es/",
+
+    'category': 'BIM',
+
+    'depends': [
+
+        'base',
+        'bim_project',
+        'base_bim_2',
+
+    ],
+
+    'data': [
+       
+        'views/bim_project.xml',
+                   
+    ],
+    'installable': True
+}
